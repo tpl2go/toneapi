ccdeed8398180b4a6bfbda41d22c24cd0dcd3d84
Ping Liang Tan <tpl2go@gmail.com>
2024-06-23 01:25:48 +0800

    WIP fixed accuracy functions

--- include/tipps_fixedaccuracy.hpp
+++ include/tipps_fixedaccuracy.hpp
@@ -81,6 +81,91 @@ namespace ipp
         IppStatus ippsConj_32fc_I(Ipp32fc *pSrcDst, int len);
         IppStatus ippsConj_64fc_I(Ipp64fc *pSrcDst, int len);
 
+        IppStatus ippsCos_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsCos_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+        IppStatus ippsCos_32fc_A24(const Ipp32fc *pSrc, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsCos_64fc_A53(const Ipp64fc *pSrc, Ipp64fc *pDst, Ipp32s len);
+
+        IppStatus ippsCosh_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsCosh_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+        IppStatus ippsCosh_32fc_A24(const Ipp32fc *pSrc, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsCosh_64fc_A53(const Ipp64fc *pSrc, Ipp64fc *pDst, Ipp32s len);
+
+        IppStatus ippsErf_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsErf_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsErfInv_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsErfInv_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsErfc_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsErfc_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsErfcInv_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsErfcInv_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        // IppStatus ippsExp_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        // IppStatus ippsExp_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+        IppStatus ippsExp_32fc_A24(const Ipp32fc *pSrc, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsExp_64fc_A53(const Ipp64fc *pSrc, Ipp64fc *pDst, Ipp32s len);
+
+        // IppStatus ippsDiv_32f_A24(const Ipp32f *pSrc1, const Ipp32f *pSrc2, Ipp32f *pDst, Ipp32s len);
+        // IppStatus ippsDiv_64f_A53(const Ipp64f *pSrc1, const Ipp64f *pSrc2, Ipp64f *pDst, Ipp32s len);
+        // IppStatus ippsDiv_32fc_A24(const Ipp32fc *pSrc1, const Ipp32fc *pSrc2, Ipp32fc *pDst, Ipp32s len);
+        // IppStatus ippsDiv_64fc_A53(const Ipp64fc *pSrc1, const Ipp64fc *pSrc2, Ipp64fc *pDst, Ipp32s len);
+
+        IppStatus ippsExpm1_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsExpm1_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsFloor_32f(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsFloor_64f(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsFrac_32f(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsFrac_64f(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsHypot_32f_A24(const Ipp32f *pSrc1, const Ipp32f *pSrc2, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsHypot_64f_A53(const Ipp64f *pSrc1, const Ipp64f *pSrc2, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsInvCbrt_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsInvCbrt_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsInvSqrt_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsInvSqrt_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsInv_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsInv_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        // IppStatus ippsLn_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        // IppStatus ippsLn_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+        IppStatus ippsLn_32fc_A24(const Ipp32fc *pSrc, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsLn_64fc_A53(const Ipp64fc *pSrc, Ipp64fc *pDst, Ipp32s len);
+
+        IppStatus ippsLog10_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsLog10_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+        IppStatus ippsLog10_32fc_A24(const Ipp32fc *pSrc, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsLog10_64fc_A53(const Ipp64fc *pSrc, Ipp64fc *pDst, Ipp32s len);
+
+        IppStatus ippsLog1p_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsLog1p_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsModf_32f(const Ipp32f *pSrc, Ipp32f *pDst1, Ipp32f *pDst2, Ipp32s len);
+        IppStatus ippsModf_64f(const Ipp64f *pSrc, Ipp64f *pDst1, Ipp64f *pDst2, Ipp32s len);
+
+        IppStatus ippsMulByConj_32fc_A24(const Ipp32fc *pSrc1, const Ipp32fc *pSrc2, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsMulByConj_64fc_A53(const Ipp64fc *pSrc1, const Ipp64fc *pSrc2, Ipp64fc *pDst, Ipp32s len);
+
+        IppStatus ippsNearbyInt_32f(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsNearbyInt_64f(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsPow2o3_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsPow2o3_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsPow3o2_32f_A24(const Ipp32f *pSrc, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsPow3o2_64f_A53(const Ipp64f *pSrc, Ipp64f *pDst, Ipp32s len);
+
+        IppStatus ippsPow_32f_A24(const Ipp32f *pSrc1, const Ipp32f *pSrc2, Ipp32f *pDst, Ipp32s len);
+        IppStatus ippsPow_64f_A53(const Ipp64f *pSrc1, const Ipp64f *pSrc2, Ipp64f *pDst, Ipp32s len);
+        IppStatus ippsPow_32fc_A24(const Ipp32fc *pSrc1, const Ipp32fc *pSrc2, Ipp32fc *pDst, Ipp32s len);
+        IppStatus ippsPow_64fc_A53(const Ipp64fc *pSrc1, const Ipp64fc *pSrc2, Ipp64fc *pDst, Ipp32s len);
         /**
  *
  *
@@ -88,189 +173,6 @@ namespace ipp
 
 
 
-
-                  ippsCeil_32f
-                  ippsCeil_64f
-                  ippsConj_32fc_A24
-                  ippsConj_64fc_A53
-                  ippsCos_32f_A11
-                  ippsCos_32f_A21
-                  ippsCos_32f_A24
-                  ippsCos_32fc_A11
-                  ippsCos_32fc_A21
-                  ippsCos_32fc_A24
-                  ippsCos_64f_A26
-                  ippsCos_64f_A50
-                  ippsCos_64f_A53
-                  ippsCos_64fc_A26
-                  ippsCos_64fc_A50
-                  ippsCos_64fc_A53
-                  ippsCosh_32f_A11
-                  ippsCosh_32f_A21
-                  ippsCosh_32f_A24
-                  ippsCosh_32fc_A11
-                  ippsCosh_32fc_A21
-                  ippsCosh_32fc_A24
-                  ippsCosh_64f_A26
-                  ippsCosh_64f_A50
-                  ippsCosh_64f_A53
-                  ippsCosh_64fc_A26
-                  ippsCosh_64fc_A50
-                  ippsCosh_64fc_A53
-                  ippsDiv_32f_A11
-                  ippsDiv_32f_A21
-                  ippsDiv_32f_A24
-                  ippsDiv_32fc_A11
-                  ippsDiv_32fc_A21
-                  ippsDiv_32fc_A24
-                  ippsDiv_64f_A26
-                  ippsDiv_64f_A50
-                  ippsDiv_64f_A53
-                  ippsDiv_64fc_A26
-                  ippsDiv_64fc_A50
-                  ippsDiv_64fc_A53
-                  ippsErfInv_32f_A11
-                  ippsErfInv_32f_A21
-                  ippsErfInv_32f_A24
-                  ippsErfInv_64f_A26
-                  ippsErfInv_64f_A50
-                  ippsErfInv_64f_A53
-                  ippsErf_32f_A11
-                  ippsErf_32f_A21
-                  ippsErf_32f_A24
-                  ippsErf_64f_A26
-                  ippsErf_64f_A50
-                  ippsErf_64f_A53
-                  ippsErfcInv_32f_A11
-                  ippsErfcInv_32f_A21
-                  ippsErfcInv_32f_A24
-                  ippsErfcInv_64f_A26
-                  ippsErfcInv_64f_A50
-                  ippsErfcInv_64f_A53
-                  ippsErfc_32f_A11
-                  ippsErfc_32f_A21
-                  ippsErfc_32f_A24
-                  ippsErfc_64f_A26
-                  ippsErfc_64f_A50
-                  ippsErfc_64f_A53
-                  ippsExp_32f_A11
-                  ippsExp_32f_A21
-                  ippsExp_32f_A24
-                  ippsExp_32fc_A11
-                  ippsExp_32fc_A21
-                  ippsExp_32fc_A24
-                  ippsExp_64f_A26
-                  ippsExp_64f_A50
-                  ippsExp_64f_A53
-                  ippsExp_64fc_A26
-                  ippsExp_64fc_A50
-                  ippsExp_64fc_A53
-                  ippsExpm1_32f_A11
-                  ippsExpm1_32f_A21
-                  ippsExpm1_32f_A24
-                  ippsExpm1_64f_A26
-                  ippsExpm1_64f_A50
-                  ippsExpm1_64f_A53
-                  ippsFloor_32f
-                  ippsFloor_64f
-                  ippsFrac_32f
-                  ippsFrac_64f
-                  ippsHypot_32f_A11
-                  ippsHypot_32f_A21
-                  ippsHypot_32f_A24
-                  ippsHypot_64f_A26
-                  ippsHypot_64f_A50
-                  ippsHypot_64f_A53
-                  ippsInvCbrt_32f_A11
-                  ippsInvCbrt_32f_A21
-                  ippsInvCbrt_32f_A24
-                  ippsInvCbrt_64f_A26
-                  ippsInvCbrt_64f_A50
-                  ippsInvCbrt_64f_A53
-                  ippsInvSqrt_32f_A11
-                  ippsInvSqrt_32f_A21
-                  ippsInvSqrt_32f_A24
-                  ippsInvSqrt_64f_A26
-                  ippsInvSqrt_64f_A50
-                  ippsInvSqrt_64f_A53
-                  ippsInv_32f_A11
-                  ippsInv_32f_A21
-                  ippsInv_32f_A24
-                  ippsInv_64f_A26
-                  ippsInv_64f_A50
-                  ippsInv_64f_A53
-                  ippsLn_32f_A11
-                  ippsLn_32f_A21
-                  ippsLn_32f_A24
-                  ippsLn_32fc_A11
-                  ippsLn_32fc_A21
-                  ippsLn_32fc_A24
-                  ippsLn_64f_A26
-                  ippsLn_64f_A50
-                  ippsLn_64f_A53
-                  ippsLn_64fc_A26
-                  ippsLn_64fc_A50
-                  ippsLn_64fc_A53
-                  ippsLog10_32f_A11
-                  ippsLog10_32f_A21
-                  ippsLog10_32f_A24
-                  ippsLog10_32fc_A11
-                  ippsLog10_32fc_A21
-                  ippsLog10_32fc_A24
-                  ippsLog10_64f_A26
-                  ippsLog10_64f_A50
-                  ippsLog10_64f_A53
-                  ippsLog10_64fc_A26
-                  ippsLog10_64fc_A50
-                  ippsLog10_64fc_A53
-                  ippsLog1p_32f_A11
-                  ippsLog1p_32f_A21
-                  ippsLog1p_32f_A24
-                  ippsLog1p_64f_A26
-                  ippsLog1p_64f_A50
-                  ippsLog1p_64f_A53
-                  ippsModf_32f
-                  ippsModf_64f
-                  ippsMulByConj_32fc_A11
-                  ippsMulByConj_32fc_A21
-                  ippsMulByConj_32fc_A24
-                  ippsMulByConj_64fc_A26
-                  ippsMulByConj_64fc_A50
-                  ippsMulByConj_64fc_A53
-                  ippsMul_32f_A24
-                  ippsMul_32fc_A11
-                  ippsMul_32fc_A21
-                  ippsMul_32fc_A24
-                  ippsMul_64f_A53
-                  ippsMul_64fc_A26
-                  ippsMul_64fc_A50
-                  ippsMul_64fc_A53
-                  ippsNearbyInt_32f
-                  ippsNearbyInt_64f
-                  ippsPow2o3_32f_A11
-                  ippsPow2o3_32f_A21
-                  ippsPow2o3_32f_A24
-                  ippsPow2o3_64f_A26
-                  ippsPow2o3_64f_A50
-                  ippsPow2o3_64f_A53
-                  ippsPow3o2_32f_A11
-                  ippsPow3o2_32f_A21
-                  ippsPow3o2_32f_A24
-                  ippsPow3o2_64f_A26
-                  ippsPow3o2_64f_A50
-                  ippsPow3o2_64f_A53
-                  ippsPow_32f_A11
-                  ippsPow_32f_A21
-                  ippsPow_32f_A24
-                  ippsPow_32fc_A11
-                  ippsPow_32fc_A21
-                  ippsPow_32fc_A24
-                  ippsPow_64f_A26
-                  ippsPow_64f_A50
-                  ippsPow_64f_A53
-                  ippsPow_64fc_A26
-                  ippsPow_64fc_A50
-                  ippsPow_64fc_A53
                   ippsPowx_32f_A11
                   ippsPowx_32f_A21
                   ippsPowx_32f_A24
